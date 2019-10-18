2f807305504a227a81504f859e7ceedfa18646c3
Vladislav <arctic.monkeys.91ru@gmail.com>
2019-10-19 01:51:42 +0300

    pre release

--- web.py
+++ web.py
@@ -1,6 +1,22 @@
-import webbrowser
-webbrowser.open('http://onliner.by', new=2)
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 
-# from selenium import webdriver
-# driver = webdriver.Chrome()
-# driver.get("http://www.python.org")
+driver = webdriver.Chrome()
+driver.get( "http://www.onliner.by" )
+assert "Onliner" in driver.title
+elem = driver.find_element_by_xpath( "//*[@id='fast-search']/form/input[1]" )
+elem.send_keys('Iphone')
+
+#elem = driver.find_element_by_xpath( "//div[@id='search-page']/div[@class='search__bar']//input" )
+#elem.send_keys('iPhone 6')
+
+driver.get("https://catalog.onliner.by/mobile/apple/iphone632gbg")
+driver.get("https://catalog.onliner.by/mobile/apple/iphone632gbg/prices?town=minsk&order=shop_rating:desc")
+
+#browser = webdriver.Chrome()
+#browser.find_elements_by_xpath("//div[@id='search-page']/div[@class='search__bar']//input")
+
+
+time.sleep(5)
+driver.close()
