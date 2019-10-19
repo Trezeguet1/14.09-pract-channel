9c8802fe3f840d17f19692f164c42a11ad3b3856
Vladislav <arctic.monkeys.91ru@gmail.com>
2019-10-19 10:41:48 +0300

    release

--- web.py
+++ web.py
@@ -3,20 +3,21 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 driver = webdriver.Chrome()
+
 driver.get( "http://www.onliner.by" )
 assert "Onliner" in driver.title
+
 elem = driver.find_element_by_xpath( "//*[@id='fast-search']/form/input[1]" )
 elem.send_keys('Iphone')
 
-#elem = driver.find_element_by_xpath( "//div[@id='search-page']/div[@class='search__bar']//input" )
-#elem.send_keys('iPhone 6')
+time.sleep(3)
 
-driver.get("https://catalog.onliner.by/mobile/apple/iphone632gbg")
-driver.get("https://catalog.onliner.by/mobile/apple/iphone632gbg/prices?town=minsk&order=shop_rating:desc")
+driver.switch_to.frame(driver.find_element_by_css_selector(".modal-iframe"))
+iphone = driver.find_element_by_xpath("//*[@id='search-page']/div[2]/ul/li[1]/div/div/div[2]/div/a")
+iphone.click()
 
-#browser = webdriver.Chrome()
-#browser.find_elements_by_xpath("//div[@id='search-page']/div[@class='search__bar']//input")
-
+if driver.find_elements_by_css_selector(".button.button_big.button_orange.offers-description__button"):
+    print("ДОСТУПЕН ДЛЯ ПОКУПКИ")
 
 time.sleep(5)
 driver.close()
